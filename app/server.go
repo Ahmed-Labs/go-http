48977923b883abc7012d6889222bb218c6e03b75
Ahmed-Labs <ahmedelhagjr@gmail.com>
2024-07-21 13:37:06 -0400

    Update response writing to simplify

--- app/server.go
+++ app/server.go
@@ -6,27 +6,6 @@ import (
 	"os"
 )
 
-const HTTP_VERSION = "HTTP/1.1"
-
-type header struct {
-	name  string
-	value string
-}
-type statusLine struct {
-	httpVersion string
-	statusCode  string
-	statusText  string
-}
-
-func response(headers []header, status statusLine, body string) []byte {
-	status_data := HTTP_VERSION + " " + status.statusCode + " " + status.statusText
-	header_data := ""
-	body_data := ""
-
-	resp := status_data + "\r\n" + header_data + "\r\n" + body_data
-	return []byte(resp)
-}
-
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -41,9 +20,5 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	_, err = conn.Write(response([]header{}, statusLine{statusCode: "200", statusText: "OK"}, ""))
-	if err != nil {
-		fmt.Println("Error writing response", err.Error())
-		os.Exit(1)
-	}
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
