61da1d2f3ca3ba4ca249fa0a0249c5dba305d775
Ahmed-Labs <ahmedelhagjr@gmail.com>
2024-07-21 13:33:04 -0400

    pass 1st stage

--- app/server.go
+++ app/server.go
@@ -6,6 +6,27 @@ import (
 	"os"
 )
 
+const HTTP_VERSION = "HTTP/1.1"
+
+type header struct {
+	name  string
+	value string
+}
+type statusLine struct {
+	httpVersion string
+	statusCode  string
+	statusText  string
+}
+
+func response(headers []header, status statusLine, body string) []byte {
+	status_data := HTTP_VERSION + " " + status.statusCode + " " + status.statusText
+	header_data := ""
+	body_data := ""
+
+	resp := status_data + "\r\n" + header_data + "\r\n" + body_data
+	return []byte(resp)
+}
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -14,10 +35,15 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	_, err = conn.Write(response([]header{}, statusLine{statusCode: "200", statusText: "OK"}, ""))
+	if err != nil {
+		fmt.Println("Error writing response", err.Error())
 		os.Exit(1)
 	}
 }
